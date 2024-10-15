e23203c00c554fdf173a25611bfde6dda1fb6291
ahmwajih <ahmed.wajih.ben.smida@gmail.com>
2024-10-15 17:58:21 +0200

    finish the authentification

--- app/middlewares/permission.tsx
+++ app/middlewares/permission.tsx
@@ -1,25 +1,27 @@
+// import { NextRequest, NextResponse } from 'next/server';
+// import { NextHandler } from 'next/dist/server/next';
+// import session from next-session;
 
+// export const isLogin = async (req: NextRequest, res: NextResponse, next: NextHandler) => {
+//     const user = req.session.get('user');
+//     if (user) {
+//         return res.redirect('/dashboard');
+//     }
+//     return next();
+// };
 
-export islogin = async (req: NextRequest, res: NextResponse, next: NextHandler) => {
-    if (req.session.get('user')) {
-        return res.redirect('/dashboard');
-    }
-    return next();
-    }
+// export const isActive = async (req: NextRequest, res: NextResponse, next: NextHandler) => {
+//     const user = req.session.get('user');
+//     if (user && user.isActive) {
+//         return next();
+//     }
+//     return res.redirect('/login');
+// };
 
-
-export isActive = async (req: NextRequest, res: NextResponse, next: NextHandler) => {
-        if (req.session.get('user').isActive) {
-            return next();
-        }
-        return res.redirect('/login');
-    }
-
-export isAdmin = async (req: NextRequest, res: NextResponse, next: NextHandler) => {
-        if (req.session.get('user').role === 'admin') {
-            return next();
-        }
-        return res.redirect('/dashboard');
-    }
-
-
+// export const isAdmin = async (req: NextRequest, res: NextResponse, next: NextHandler) => {
+//     const user = req.session.get('user');
+//     if (user && user.role === 'admin') {
+//         return next();
+//     }
+//     return res.redirect('/dashboard');
+// };

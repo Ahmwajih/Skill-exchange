d0a368348b17fd5d1b40574b2af546ab704f7c94
ahmwajih <ahmed.wajih.ben.smida@gmail.com>
2024-12-14 16:04:43 +0100

    firebase to .env fine

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,14 +1,17 @@
 // lib/firebase.ts
 import { initializeApp, getApp, getApps } from 'firebase/app';
 import { getAuth, GoogleAuthProvider, GithubAuthProvider } from 'firebase/auth';
+import dotenv from 'dotenv';
+
+dotenv.config({ path: '.env' });
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDtiRFLDUErXZ3jHBqzhmJu8Yzo_LefI44",
-  authDomain: "skillexchange-35492.firebaseapp.com",
-  projectId: "skillexchange-35492",
-  storageBucket: "skillexchange-35492.appspot.com",
-  messagingSenderId: "250514866295",
-  appId: "G-K5TXN6WNWB"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
